76b9d690ade5026adff6c5a90fe116b3b47e7f0c
Bishnu Prasad pandey <pandeybinod2075@gmail.com>
2023-01-17 10:07:22 +0545

    adding some new class

--- src/main/java/com/SpringAllCrudOperation/SpringSecurityApplication.java
+++ src/main/java/com/SpringAllCrudOperation/SpringSecurityApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.converter.json.GsonBuilderUtils;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -13,6 +16,67 @@ public class SpringSecurityApplication {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 
 		System.out.println("git branch check");
+//		int i,size;
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter size of array");
+//		size=sc.nextInt();
+//		int a []=new int[size];
+//		System.out.println("enter array element");
+//		for (i=0;i<size;i++){
+//			a[i]=sc.nextInt();
+//		}
+//		System.out.println("enter element are :");
+//		for(i=0;i<size;i++){
+//			System.out.println(a[i]);
+		System.out.println("**************");
+		//Array list
+		/*
+		ArrayList ma memory non continuous allocation hunx tar array ma continuous
+		arrayList varible size hunx bhane array fixed size
+		 */
+		ArrayList<Integer> list=new ArrayList<>();
+		//add element
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(2);
+		System.out.println(list);
+		//remove element
+		list.remove(2);
+		System.out.println(list);
+		//element get by index
+		int a=list.get(1);
+		System.out.println(a);
+		//add element in between
+		list.add(1,4);
+		System.out.println(list);
+		//modify or set element
+		list.set(3,2);
+		System.out.println(list);
+		//size of Arraylist
+		int b=list.size();
+		System.out.println(b);
+		//loop
+		for (int i=0;i< list.size();i++)
+			System.out.println(list.get(i));
+
+     //sorting
+		Collections.sort(list);
+		System.out.println(list);
+		System.out.println("**************");
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		Double d=scan.nextDouble();
+		String s=scan.nextLine();
+
+		// Write your code here.
+
+		System.out.println("String: " + s);
+		System.out.println("Double: " + d);
+		System.out.println("Int: " + i);
+		System.out.println("*****************");
+
+
 /*
 //Program for Paliandrome number;
 	int n,s=0,r,c;
